30b1b43c6216f63fdb78d193aa78fa2309fa8d09
Natalie Ayers <natra6628@gmail.com>
2021-02-20 17:26:31 -0700

    cleaning up mapquest_ap.py file

--- Python/mapquest_ap.py
+++ Python/mapquest_ap.py
@@ -3,37 +3,42 @@ import json
 import pandas as pd
 import re
 
+
+# Format addresses pulled from ME records; add hash key
 raw = pd.read_csv('C:/Users/natra/Documents/ME_2021/GunViolence_ME/python_addresses.csv')
 for row, index in zip(raw.values, raw.index):   
-    raw.loc[index, 'str_address'] = re.sub('\.', '', re.sub(r'\s','+',raw.loc[index,'ADDRESS']))
-    raw.loc[index, 'keystr'] = re.sub(r'\s','', "".join([str(i) for i in row if not pd.isna(i)]))
+    raw.loc[index, 'str_address'] = re.sub('\.', '', 
+        re.sub(r'\s','+',raw.loc[index,'ADDRESS']))
+    raw.loc[index, 'keystr'] = re.sub(r'\s','', 
+        "".join([str(i) for i in row if not pd.isna(i)]))
 raw.loc[:,'HASH_KEY'] = pd.util.hash_pandas_object(raw.loc[:,'keystr'])
 
-#url = ("http://www.mapquestapi.com/geocoding/v1/address?key={}&street={}"
-#"&county={}&state={}&thumbMaps=false")
-url = ("http://www.mapquestapi.com/geocoding/v1/address?key={}&street={}"
-"&county={}&state={}&thumbMaps=false&maxResults=3")
-apikey = "tRQRsDSxzHGAGzyVqlgA9ppFa1a2gYcj"
-request = url.format(apikey, raw.loc[0,'str_address'], raw.loc[0,'COUNTY'],raw.loc[0,'STATE'])
-
-test_5box = json.loads(requests.get(request).text)
-
-
+# Build dynamic request
 url = ("http://www.mapquestapi.com/geocoding/v1/address?key={}&street={}"
 "&county={}&state={}&thumbMaps=false&maxResults=3")
 apikey = "tRQRsDSxzHGAGzyVqlgA9ppFa1a2gYcj"
 
+# Use MapQuest Geocoding API to pull locations for all addresses
 full_locs = {}
-for i in range(500,3254):
-    request = url.format(apikey, raw.loc[i,'str_address'], raw.loc[i,'COUNTY'],raw.loc[i,'STATE'])
+for i in range(raw.shape[0]):
+    request = url.format(apikey, raw.loc[i,'str_address'], 
+                        raw.loc[i,'COUNTY'],raw.loc[i,'STATE'])
     resp = json.loads(requests.get(request).text)
     full_locs[str(raw.loc[i,'HASH_KEY'])] = resp
 
+# Load addresses with locations into json
 with open("address_locs.json","w") as f:
     json.dump(full_locs, f, indent=4)
 
 
-
+"""
+### Single, 5-box address request for testing ###
+url = ("http://www.mapquestapi.com/geocoding/v1/address?key={}&street={}"
+"&county={}&state={}&thumbMaps=false&maxResults=3")
+apikey = "tRQRsDSxzHGAGzyVqlgA9ppFa1a2gYcj"
+request = url.format(apikey, raw.loc[0,'str_address'], 
+                    raw.loc[0,'COUNTY'],raw.loc[0,'STATE'])
+test_5box = json.loads(requests.get(request).text)
 
 
 
@@ -46,3 +51,4 @@ url = "http://www.mapquestapi.com/geocoding/v1/address?key={}&location={}$thumbM
 apikey = "tRQRsDSxzHGAGzyVqlgA9ppFa1a2gYcj"
 request = url.format(apikey, raw.loc[0,'address'])
 test = json.loads(requests.get(request).text)
+"""
